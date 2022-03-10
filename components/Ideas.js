33335e81f76dd6a335c1bded5e2da954d8dd95d0
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-03-10 11:00:44 -0600

    Added navigation

--- components/Ideas.js
+++ components/Ideas.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Review() {
+export default function Ideas() {
+    return (
+        <View>
 
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
