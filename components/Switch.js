5d8e74e4c2a28961ee70518080b87d3a363b64a8
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-03-16 23:02:40 -0500

    Modded Switch

--- components/Switch.js
+++ components/Switch.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Switch() {
+export default function Switch(props) {
+    return (
+        <View>
 
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+
+})
