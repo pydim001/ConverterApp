764c222b799a2e37b08974cca9f20ffe71af7df9
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-03-31 18:54:51 -0500

    Modded Settings Switch

--- components/SettingSwitch.js
+++ components/SettingSwitch.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch } from "react-native";
 
-export default function SettingSwitch() {
+export default function SettingSwitch(props) {
+
+    const [mode, setMode] = useState(props.trackMode["off"]);
 
     return (
         <View>
-
+            <Switch
+                trackColor={props.trackColor}
+                thumbColor={props.thumbColor}
+                onValueChange={setMode(curr => !curr)}
+            />
         </View>
     )
 }
