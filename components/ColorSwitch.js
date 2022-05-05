07a1f2850019335886996744c78742d05e6d2477
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-05-05 17:29:47 -0500

    Color palette

--- components/ColorSwitch.js
+++ components/ColorSwitch.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { View } from 'react-native';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function ColorSwitch() {
+export default function ColorSwitch(props) {
+
+    const palette = () => {
+
+    }
 
     return (
         <View>
-
+            <TouchableOpacity
+                onPress={palette}
+                style={styles.palette}>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    palette: {
+        marginTop: -30,
+        marginLeft: 260,
+        width: 30,
+        height: 30,
+        backgroundColor: 'lightblue',
+        borderWidth: 2
+    }
+})
